ea2edde92b4e5532344949e8f86a81f2bd662a1e
fenghuibin <fenghuibin@ismartv.cn>
2016-04-06 15:34:14 +0800

    init project

---
+++ app/src/main/java/cn/ismartv/rotatabletextviewsample/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.ismartv.rotatabletextviewsample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.ismartv.rotatabletextview.RotatableTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
